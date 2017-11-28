93031e48499ac60546ded7af1883661e2fa8e50f
sufa bitton <sufabitton@sufas-imac.mynet>
2017-11-28 16:05:40 +0200

    selection confirmation: add selection confirmation to party selection window
    at the selection window after user press done a popup is shown for confirmation.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('flask_config')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
 
 from app import views, models
